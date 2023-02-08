240c3e66a9be03de7927953bcc6d6f744333f783
Sonal Sekhda <ssekhda@ggc.edu>
2023-02-07 22:01:12 -0500

    initial commit

---
+++ src/BodyMassIndex.java
@@ -0,0 +1,35 @@
+/**
+ * @class : BodyMassIndex
+ * @author: Sonal Sekhda
+ * @course: ITEC 2140 - 04 Spring 2023
+ * @date: February 7, 2023
+ * Write a program that compute BMI using formula BMI = weight(kilograms )/ height(meter * meter)
+ */
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the value of weight : ");
+        double weight = scan.nextDouble(); //Declare the variable weight in pound
+        System.out.print("Enter the value of the height : ");
+        double height = scan.nextDouble(); //Decalre tne variable height in inches
+        weight = weight * 0.4536; //Convert weight pounds to kilograms
+        System.out.println("Weight in Kilograms : " + weight);
+        height = height * 0.0254; //Convert height inches to meters
+        System.out.println("Height is in the meter: " + height);
+        double bodyMassIndex; //Declare the variable BodyMassIndex
+        bodyMassIndex = weight / (height * height);// count BodyMassIndex
+        System.out.printf("Total BodyMassIndex is : %.4f ", bodyMassIndex);
+        System.out.println();
+
+        if (bodyMassIndex >= 30.0) {
+            System.out.println("Obese");
+        } else if (bodyMassIndex < 30.0 && bodyMassIndex >= 25.0) {
+            System.out.println("Overweight");
+        } else if (bodyMassIndex < 25 && bodyMassIndex >= 18.5) {
+            System.out.println("Normal");
+        } else
+            System.out.println("Underweight");
+    }
+}
---
+++ src/PerimeterOfTriangle.java
@@ -0,0 +1,25 @@
+/**
+ *  @class : PerimeterOfTriangle
+ *  @author: Sonal Sekhda
+ *  @course: ITEC 2140 - 04 Spring 2023
+ *  @date: February 7, 2023
+ *  * Write a program about perimeter of the triangle with condition every pair of the two edges greater than third
+ */
+import java.util.Scanner;
+public class PerimeterOfTriangle {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the side a of the triangle : ");
+        int a = in.nextInt();
+        System.out.print("Enter the side b of the  triangle : ");
+        int b = in.nextInt();
+        System.out.print("Enter the side c of the triangle : ");
+        int c = in.nextInt();
+        int perimeter = a + b + c;
+        if(a < b + c && b < a + c && c < a + b)
+            System.out.println(perimeter + " is a valid perimeter of the triangle. ");
+        else
+            System.out.println("Invalid input ");
+    }
+
+}
